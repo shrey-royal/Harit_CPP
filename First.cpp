03642817df4a0847de34294ae36da66fbcf7c4a3
Shrey k <srkkadia@gmail.com>
2022-07-31 17:19:58 +0530

    Intro to CPP

---
+++ First.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!";
+    return 0;
+}
+
+/*
+What is cout function?
+cout stands for console output.
+cout is a function that prints the content of the variable to the screen.
+
+Syntax of cout function is as follows
+cout << "Hello World!";
+
+cout << a << "\n" << b << endl << c;
+
+
+endl function is called end line.
+endl is a function that prints the content of the variable to the screen and moves to the next line.
+
+cin function is called console input.
+
+cin >> a;   //inputs the value of a from the console.
+
+cin >> a >> b; //inputs the value of a and b from the console.
+
+*/
---
+++ basic_functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    
+    cout << endl << "a = " << a << endl << "b = " << b << endl;
+
+    return 0;
+}

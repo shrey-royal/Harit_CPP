6dd9f84068f43f8cbba2fc2f17dea27a4418beea
Shrey K <srkkadia@gmail.com>
2022-09-03 18:07:55 +0530

    Overloading Done

--- OOPs/Polymorphism/Method_Overloading.cpp
+++ OOPs/Polymorphism/Method_Overloading.cpp
@@ -5,19 +5,37 @@
 using namespace std;
 
 class Method_Overloading {
+    private:
+        int num;
+    
     public:
-        Method_Overloading() {
-            cout << "Default Constructor" << endl;
+        Method_Overloading() : num(10){}//Default Constructor
+
+        void operator ++() {
+            num += 2;
         }
 
-        Method_Overloading(int a) {
-            cout << "Overloaded Parameterized Constructor" << endl;
+        void print() {
+            cout << "The value of num is: " << num << endl;
         }
+
+
+        // Method_Overloading() {
+        //     cout << "Default Constructor" << endl;
+        // }
+
+        // Method_Overloading(int a) {
+        //     cout << "Overloaded Parameterized Constructor" << endl;
+        // }
+
+
 };
 
 int main()
 {
     //Constructor Overloading
-    Method_Overloading obj, obj1(10);
+    Method_Overloading obj;
+    ++obj;  //calling function "void operator ++()"
+    obj.print();
     return 0;
 }

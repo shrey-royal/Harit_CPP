010b6223f39c3d70d2c43f4231d9c255bc2e5f30
Shrey K <srkkadia@gmail.com>
2022-09-10 12:57:22 +0530

    string functions over

--- Strings/String_concat.cpp
+++ Strings/String_concat.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
 
+/*
 int main(){
 
     string str1, str2;
@@ -16,8 +17,28 @@ int main(){
 
     cout << "str1 is " << str1 << endl;
 
-    
 
+    return 0;
+}
+*/
+
+int main(){
+    char str1[10], str2[10];
+
+    cout << "Enter string 1: ";
+    gets(str1);
+
+    cout << "Enter string 2: ";
+    gets(str2);
+
+    //concatenate str1 and str2
+    // strcat(str1, str2);
+
+    for(int i = 0; i < strlen(str2); i++){
+        str1[strlen(str1) + i] = str2[i];
+    }
+
+    cout << "str1 is " << str1 << endl;
 
     return 0;
 }

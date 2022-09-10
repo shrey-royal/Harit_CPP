2ea674adb5058546354ff703c9a054a2576d81c1
Shrey K <srkkadia@gmail.com>
2022-09-10 13:27:51 +0530

    Exception

---
+++ Exception/exec.cpp
@@ -0,0 +1,44 @@
+/*
+
+Q. What is Exception?
+A. Exception is a run time error which occurs when the program is running.
+
+Exception Handling in cpp is a mechanism to handle the run time errors.
+
+> try block is used to check the exception.
+> catch block is used to handle the exception.
+> throw block is used to throw the exception.
+> finally block is used to execute the code after try and catch block.
+
+
+*/
+
+#include<iostream>
+#include<exception>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    try{
+        if(b == 0){
+            throw "Division by zero is not possible";
+        }
+        cout << "a/b = " << a/b << endl;
+    } catch(const char* msg) {
+        cerr << msg << endl;
+    }
+
+    cout << "End of program" << endl;
+
+    return 0;    
+}
+
+/*
+you have to take one int variable and scans a value for that variable if the value is out of the range of int then it will throw an exception and catch that exception in the catch block and print "out of range" and if the value is in the range of int then it will print the value of that variable.
+*/
